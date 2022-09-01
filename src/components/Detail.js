901b705f8a29960e9000d062cabd1673cbc13dc2
Mohammad Aquib <108718324+Md-Aquib@users.noreply.github.com>
2022-09-01 09:04:38 +0530

    Updated

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,97 +1,132 @@
 import React, {useState} from 'react'
-import {Card,Container,Form,Button,Modal} from 'react-bootstrap'
+import {Card,Container,Form,Button,Modal,Alert} from 'react-bootstrap'
 import logo from '../github_logo.png'
+import house from '../house.jpg'
 
 function Detail() {
   const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [price, Setprice] = useState('');
+  const [msg, Setmsg] = useState('');
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const close = () => setShow2(false);
+
+  let handlesubmit = async(e) => {
+    e.preventDefault()
+    Setmsg("Please Wait !!!")
+    let response = await fetch('https://house-price-prediction-backend.herokuapp.com/api/details/', {
+      method:'POST',
+      headers:{
+          'Content-Type':'application/json'
+      },
+      body:JSON.stringify({'crim': e.target.crim.value,'zn':e.target.zn.value,'indus':e.target.indus.value,
+      'chas':e.target.chas.value,'nox':e.target.nox.value,'rm':e.target.rm.value,
+      'age':e.target.age.value,'dis':e.target.dis.value,'rad':e.target.rad.value,
+      'tax':e.target.tax.value,'ptratio':e.target.ptratio.value,'b':e.target.b.value,
+      'lstat':e.target.lstat.value})
+    })
+    let data = await response.json()
+    Setprice(data.val)
+    setShow2(true);
+    Setmsg("")
+  }
+
   return (
     <div>
         <Container className='container d-flex align-items-center justify-content-center'>
             <Card className="w-100 shadow-lg rounded bg-dark opacity-50" style={{border:'none'}}>
                 <Card.Body>
+                    {msg && <Alert variant='warning'>{msg}</Alert>}
+                    {price && 
+                    <Modal show={show2} onHide={close}>
+                      <Modal.Header closeButton>
+                        <Modal.Title className='text-center'><img src={house} alt="house" style={{height:'50px',widows:'50px'}} /> House-Price-Predictor</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body style={{fontWeight:'600'}}>The total price of the house is. $ {price}.</Modal.Body>
+                    </Modal>    
+                    }
                     <Button className='info btn btn-light' variant="primary" onClick={handleShow}>
                       ?
                     </Button>
-                    <Form>
+                    <Form onSubmit={handlesubmit}>
                       <div className="row">
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >CRIM</label>
-                            <input required type="number" className='form-control' name="crim" />
+                            <input required type="number" step="any" className='form-control' name="crim" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >ZN</label>
-                            <input required type="number" className='form-control' name="zn" />
+                            <input required type="number" step="any" className='form-control' name="zn" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >INDUS</label>
-                            <input required type="number" className='form-control' name="indus" />
+                            <input required type="number" step="any" className='form-control' name="indus" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >CHAS</label>
-                            <input required type="number" className='form-control' name="chas" />
+                            <input required type="number" step="any" className='form-control' name="chas" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >NOX</label>
-                            <input required type="number" className='form-control' name="nox" />
+                            <input required type="number" step="any" className='form-control' name="nox" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >RM</label>
-                            <input required type="number" className='form-control' name="rm" />
+                            <input required type="number" step="any" className='form-control' name="rm" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >AGE</label>
-                            <input required type="number" className='form-control' name="age" />
+                            <input required type="number" step="any" className='form-control' name="age" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >DIS</label>
-                            <input required type="number" className='form-control' name="dis" />
+                            <input required type="number" step="any" className='form-control' name="dis" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >RAD</label>
-                            <input required type="number" className='form-control' name="rad" />
+                            <input required type="number" step="any" className='form-control' name="rad" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >TAX</label>
-                            <input required type="number" className='form-control' name="tax" />
+                            <input required type="number" step="any" className='form-control' name="tax" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >PTRATIO</label>
-                            <input required type="number" className='form-control' name="ptratio" />
+                            <input required type="number" step="any" className='form-control' name="ptratio" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >B</label>
-                            <input required type="number" className='form-control' name="b" />
+                            <input required type="number" step="any" className='form-control' name="b" />
                           </Form.Group>
                         </div>
                         <div className='col-lg-4 col-md-6 col-12'>
                           <Form.Group>
                             <label >LSTAT</label>
-                            <input required type="number" className='form-control' name="lstat" />
+                            <input required type="number" step="any" className='form-control' name="lstat" />
                           </Form.Group>
                         </div>
                       </div>
